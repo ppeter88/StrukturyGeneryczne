0fcab61df4640b1df8b1577924fb697d5be1f03d
Piotr Pikuła <p.pikula88@gmail.com>
2020-02-26 22:39:03 +0100

    1_3 NaszaKolekcja

--- CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
@@ -10,6 +10,28 @@ namespace _1_TypyGeneryczne
     {
         static void Main(string[] args)
         {
+            KolejkaKolowa kolejka = new KolejkaKolowa(5);
+
+            kolejka.Zapisz(1);
+            kolejka.Zapisz(2);
+            kolejka.Zapisz(3);
+            kolejka.Zapisz(4);
+            kolejka.Zapisz(5);
+            kolejka.Zapisz(6);
+            kolejka.Zapisz(7);
+            kolejka.Zapisz(8);
+            kolejka.Zapisz(9);
+            kolejka.Zapisz(10);
+            kolejka.Zapisz(11);
+            kolejka.Zapisz(12);
+            kolejka.Zapisz(13);
+            kolejka.Zapisz(14);
+
+            foreach (var item in kolejka.bufor)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
