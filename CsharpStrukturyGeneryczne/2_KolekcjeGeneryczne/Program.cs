496901cb5db654e7be1317f699b8500f1e593ef3
Piotr Pikuła <p.pikula88@gmail.com>
2020-03-04 21:32:55 +0100

    2_KolekcjeGeneryczne_Tablice

--- CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -10,6 +10,22 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
+            Pracownik[] pracownicy = new Pracownik[]
+            {
+                /*Inicjalizator tablicy - tablica będzie przechowywać tyle elelemntów, jakie zdefiniujemy w klamrach*/
+                new Pracownik {Imie = "Marcin", Nazwisko = "Nowak"},
+                new Pracownik {Imie = "Łukasz", Nazwisko = "Domaradzki"},
+                new Pracownik {Imie = "Paweł", Nazwisko = "Igaweł"},
+            };
+
+            foreach (var pracownik in pracownicy)
+            {
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            }
+
+            /*Poniżej, możliwość zmiany wielkości tablicy*/
+            Array.Resize(ref pracownicy, 10);
+            pracownicy[9] = new Pracownik { Imie = "Anatol", Nazwisko = "Kołodziej" };
         }
     }
 }
