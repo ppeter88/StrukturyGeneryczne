ec44a2ce4e5e0a9f8cf9106ff4fe6480666713e2
Piotr Pikuła <p.pikula88@gmail.com>
2020-03-08 15:01:48 +0100

    HashSet

--- CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -10,61 +10,86 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
+            //ListaPracowników();
+            //ListaLiczb();
+            //Kolejka();
+            //Stos();
+            //HashSet();
 
-            ////------------------------------------------------------------------------ LISTA - pracowników --------------------------------------------------------------------------------------
-            //var pracownicy = new List<Pracownik>
-            //{
-            //    /*Inicjalizator tablicy - tablica będzie przechowywać tyle elelemntów, jakie zdefiniujemy w klamrach*/
-            //    new Pracownik {Imie = "Marcin", Nazwisko = "Nowak"},
-            //    new Pracownik {Imie = "Łukasz", Nazwisko = "Domaradzki"},
-            //    new Pracownik {Imie = "Paweł", Nazwisko = "Igaweł"},
-            //};
+            return;
+        }
 
-            //pracownicy.Add(new Pracownik { Imie = "Ania", Nazwisko = "ZZielonegoWzgórza" }); /*Uproszczone dodawania kolejnych pozycji - lista automatycznie zwiększa swój rozmiar*/
+        private static void HashSet()
+        {
+            //---------------------------------------------------------------------HashSet---------------------------------------------------------------------------------------
+            HashSet<Pracownik> set = new HashSet<Pracownik>();
 
-            //foreach (var pracownik in pracownicy)
-            //{
-            //    Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
-            //}
+            /*Odwołując się do tego samego obiektu, HS nie zapisze ponownie wartości*/
+            var pracownik = new Pracownik { Imie = "Paweł" };
+            set.Add(pracownik);
+            set.Add(pracownik);
 
-            //for (int i = 0; i < pracownicy.Count; i++) /*Do określania wielkości Listy, służy Count a nie Length*/
-            //{
-            //    Console.WriteLine(pracownicy[i].Imie + " " + pracownicy[i].Nazwisko);
-            //}
+            /*W poniższym przypadku, HS pozwoli wpisać Wartość ponownie - są to odrębne obiekty, HS nie potrafi sprawdzićw prosty sposób czy się różnią*/
+            set.Add(new Pracownik { Imie = "Marcin" });
+            set.Add(new Pracownik { Imie = "Marcin" });
 
+            Console.WriteLine();
 
-            ////------------------------------------------------------------------------- LISTA - liczb -----------------------------------------------------------------------------------------
-            //var liczby = new List<int>();
-            //var pojemnosc = -1;
-
-            //while (true)
-            //{
-            //    if (liczby.Capacity != pojemnosc)
-            //    {
-            //        pojemnosc = liczby.Capacity;
-            //        Console.WriteLine(pojemnosc);
-            //    }
-
-            //    liczby.Add(1);
-            //}
-
-
-            ////-------------------------------------------------------------------------- KOLEJKA ----------------------------------------------------------------------------------------------
-            Queue<Pracownik> kolejka = new Queue<Pracownik>();
-            kolejka.Enqueue(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
-            kolejka.Enqueue(new Pracownik { Imie = "Paweł", Nazwisko = "Domagała" });
-            kolejka.Enqueue(new Pracownik { Imie = "Zenek", Nazwisko = "Martyniuk" });
-            kolejka.Enqueue(new Pracownik { Imie = "Damian", Nazwisko = "Krauze" });
-
-            Console.WriteLine("Kolejka");
-
-            while(kolejka.Count > 0)
+            foreach (var item in set)
             {
-                var pracownik = kolejka.Dequeue();
+                Console.WriteLine(item.Imie);
+            }
+        }
+
+        private static void ListaPracowników()
+        {
+            //------------------------------------------------------------------------ LISTA - pracowników --------------------------------------------------------------------------------------
+            var pracownicy = new List<Pracownik>
+            {
+                /*Inicjalizator tablicy - tablica będzie przechowywać tyle elelemntów, jakie zdefiniujemy w klamrach*/
+                new Pracownik {Imie = "Marcin", Nazwisko = "Nowak"},
+                new Pracownik {Imie = "Łukasz", Nazwisko = "Domaradzki"},
+                new Pracownik {Imie = "Paweł", Nazwisko = "Igaweł"},
+            };
+
+            pracownicy.Add(new Pracownik { Imie = "Ania", Nazwisko = "ZZielonegoWzgórza" }); /*Uproszczone dodawania kolejnych pozycji - lista automatycznie zwiększa swój rozmiar*/
+
+            Console.WriteLine("LISTA - pracowników");
+            Console.WriteLine();
+            Console.WriteLine("Foreach");
+            foreach (var pracownik in pracownicy)
+            {
                 Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
             }
 
+            Console.WriteLine();
+            Console.WriteLine("For");
+            for (int i = 0; i < pracownicy.Count; i++) /*Do określania wielkości Listy, służy Count a nie Length*/
+            {
+                Console.WriteLine(pracownicy[i].Imie + " " + pracownicy[i].Nazwisko);
+            }
+        }
 
+        private static void ListaLiczb()
+        {
+            //------------------------------------------------------------------------- LISTA - liczb -----------------------------------------------------------------------------------------
+            var liczby = new List<int>();
+            var pojemnosc = -1;
+
+            while (true)
+            {
+                if (liczby.Capacity != pojemnosc)
+                {
+                    pojemnosc = liczby.Capacity;
+                    Console.WriteLine(pojemnosc);
+                }
+
+                liczby.Add(1);
+            }
+        }
+
+        private static void Stos()
+        {
             //----------------------------------------------------------------------------- STOS -------------------------------------------------------------------------------------------------
             Stack<Pracownik> stos = new Stack<Pracownik>();
             stos.Push(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
@@ -80,7 +105,25 @@ namespace _2_KolekcjeGeneryczne
                 var pracownik = stos.Pop();
                 Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
             }
+        }
 
+        private static void Kolejka()
+        {
+            ////-------------------------------------------------------------------------- KOLEJKA ----------------------------------------------------------------------------------------------
+            Queue<Pracownik> kolejka = new Queue<Pracownik>();
+            kolejka.Enqueue(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
+            kolejka.Enqueue(new Pracownik { Imie = "Paweł", Nazwisko = "Domagała" });
+            kolejka.Enqueue(new Pracownik { Imie = "Zenek", Nazwisko = "Martyniuk" });
+            kolejka.Enqueue(new Pracownik { Imie = "Damian", Nazwisko = "Krauze" });
+
+            Console.WriteLine();
+            Console.WriteLine("Kolejka");
+
+            while (kolejka.Count > 0)
+            {
+                var pracownik = kolejka.Dequeue();
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            }
         }
     }
 }
---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTest/HashSetTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_KolekcjeGeneryczneTest
+{
+    [TestClass]
+    public class HashSetTest
+    {
+        [TestMethod]
+        public void IntersectSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            /*Scalanie koelekcji, tylko wspólnych elementów między HS - odpowiednik JOIN*/
+
+            set1.IntersectWith(set2);
+
+            Assert.IsTrue(set1.SetEquals(new[] {2, 3}));
+        }
+
+        [TestMethod]
+        public void UnionSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            /*Łączenie elementów z obu setów - odpowiednik UNION. Części wspólne kosnoliduje w jedną - nie ma powtórzeń*/
+
+            set1.UnionWith(set2);
+
+            Assert.IsTrue(set1.SetEquals(new[] { 1, 2, 3, 4 }));
+        }
+
+        [TestMethod]
+        public void SymetricExceptSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            /*Wykazywanie elementów, którymi sety się różnią*/
+            set1.SymmetricExceptWith(set2);
+
+            Assert.IsTrue(set1.SetEquals(new[] { 1, 4 }));
+
+        }
+
+        [TestMethod]
+        public void RemoveSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            /*Z HS, można usuwać wybrany element*/
+            set1.Remove(1);
+
+            Assert.AreEqual(2, set1.Count);
+
+        }
+
+        [TestMethod]
+        public void RemoveWhereSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            /*Usuwanie elementów, spełniających określony warunek - wykorzystanie wyrażeń Lambda*/
+            set1.RemoveWhere(x => x > 1);
+
+            Assert.AreEqual(1, set1.Count);
+
+        }
+
+        [TestMethod]
+        public void ContainsSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+
+            /*Sprawdzanie czy kolekcja zawiera dany element*/
+            set1.Contains(3);
+
+            Assert.IsTrue(set1.Contains(3));
+
+        }
+
+    }
+}
